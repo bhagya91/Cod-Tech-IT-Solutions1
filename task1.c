d31c7b50cafa36eabedacb00b95f908e7034a4ac
bhagya91 <161318404+bhagya91@users.noreply.github.com>
2024-02-27 09:52:54 +0530

    Update task1.c

--- task1.c
+++ task1.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main() {
+    int randomNumber, guess, attempts = 0;
+
+     srand(time(NULL));
+
+      randomNumber = rand() % 100 + 1;
+
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I have picked a number between 1 and 100. Try to guess it!\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > randomNumber) {
+            printf("Too high! Try again.\n");
+        } else if (guess < randomNumber) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the number %d correctly in %d attempts!\n", randomNumber, attempts);
+        }
+    } while (guess != randomNumber);
+
+    return 0;
+}
